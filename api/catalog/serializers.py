9d9cc2542dac0dc3021b60c976f1d02ee4d8e6b7
Adèle Delamarche <adele.delamarche@gmail.com>
2017-10-04 11:33:25 +0200

    V2

--- api/catalog/serializers.py
+++ api/catalog/serializers.py
@@ -3,25 +3,6 @@ from catalog.models import Author
 
 
 class AuthorSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    first_name = serializers.CharField(max_length=100)
-    last_name = serializers.CharField(max_length=100)
-    date_of_birth = serializers.DateField(required=False)
-    date_of_death = serializers.DateField(required=False)
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Author.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Author` instance, given the validated data.
-        """
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.date_of_birth = validated_data.get('date_of_birth', instance.date_of_birth)
-        instance.date_of_death = validated_data.get('date_of_death', instance.date_of_death)
-        instance.save()
-        return instance
+    class Meta:
+        model = Author
+        fields = ('id', 'first_name', 'last_name', 'date_of_birth', 'date_of_death')
--- api/catalog/views.py
+++ api/catalog/views.py
@@ -1,52 +1,14 @@
+from rest_framework import generics
+
 from catalog.models import Author
 from api.catalog.serializers import AuthorSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
 
-class AuthorList(APIView):
-    """
-    List all authors, or create a new author.
-    """
-    def get(self, request, format=None):
-        authors = Author.objects.all()
-        serializer = AuthorSerializer(authors, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = AuthorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class AuthorList(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
 
 
-class AuthorDetail(APIView):
-    """
-    Retrieve, update or delete an author instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Author.objects.get(pk=pk)
-        except Author.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        author = self.get_object(pk)
-        serializer = AuthorSerializer(author)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        author = self.get_object(pk)
-        serializer = AuthorSerializer(author, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        author = self.get_object(pk)
-        author.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
