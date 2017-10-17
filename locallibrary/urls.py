6c0581828f89dfcb4eb71ca1d03fcb6e540fc9b6
Adèle Delamarche <adele.delamarche@gmail.com>
2017-10-17 17:26:02 +0200

    Fix PEP8 errors in file locallibrary/urls.py

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -19,35 +19,18 @@ from django.contrib import admin
 # Use include() to add URLS from the catalog application and authentication system
 from django.conf.urls import include
 
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
-
-
-urlpatterns += [
-    url(r'^catalog/', include('catalog.urls')),
-    url(r'^api/', include('api.urls')),
-]
-
-
-# Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-# Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
-urlpatterns += [
+
+
+urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
-]
-
-
-
-# Add Django site authentication urls (for login, logout, password management)
-urlpatterns += [
-    url('^accounts/', include('django.contrib.auth.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^catalog/', include('catalog.urls')),
+    url(r'^api/', include('api.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
