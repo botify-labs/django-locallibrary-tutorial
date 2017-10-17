2f3b15b8052818164e3936a008fee763fde6dd92
Adèle Delamarche <adele.delamarche@gmail.com>
2017-10-17 18:18:32 +0200

    Make AuthorSerializer inherit from ModelSerializer

--- api/catalog/serializers.py
+++ api/catalog/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from catalog.models import Author
 
 
-class AuthorSerializer(serializers.Serializer):
+class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ('id', 'first_name', 'last_name', 'date_of_birth', 'date_of_death')
